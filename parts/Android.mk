ebaf5a48497d4121db9af8b28d11bd5cb0ccd0f8
92thSTUDENT <medicspam6@gmail.com>
2022-05-13 19:39:41 +0700

    picasso: Remove PE customsettings resources
    Signed-off-by: 92thSTUDENT <medicspam6@gmail.com>

--- parts/Android.mk
+++ parts/Android.mk
@@ -23,7 +23,6 @@ LOCAL_USE_AAPT2 := true
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.core_core \
     androidx.preference_preference \
-    org.pixelexperience.settings.resources
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
