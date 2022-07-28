b0aa78fe3a28a823f191f759e38b850a302dc468
itsurboimasarou <medicspam6@gmail.com>
2022-07-28 17:25:58 +0700

    picasso: Lineage-ify

--- cherish_picasso.mk
+++ cherish_picasso.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from picasso device
 $(call inherit-product, device/xiaomi/picasso/device.mk)
 
-# Inherit some common CherishOS stuff.
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := cherish_picasso
+PRODUCT_NAME := lineage_picasso
 PRODUCT_DEVICE := picasso
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30 5G
@@ -23,12 +23,11 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Cherish stuffs
-CHERISH_BUILD_TYPE := OFFICIAL
+# Ricify
+RICE_MAINTAINER := masarou
+RICE_CHIPSET := SM7250
+SUSHI_BOOTANIMATION := 1080
 WITH_GMS := true
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.cherish.maintainer=masarou
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_ENABLE_BLUR := true
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_GAPPS_ARCH := arm64
+TARGET_FACE_UNLOCK_SUPPORTED := true
