9b068e147496c839f9f40ee1f2511b0903cbda08
itsurboimasarou <medicspam6@gmail.com>
2022-08-03 08:31:31 +0700

    picasso: overlay: Redefine overlay-cherish & overlay-lineage seperately
    Signed-off-by: itsurboimasarou <medicspam6@gmail.com>

--- device.mk
+++ device.mk
@@ -19,7 +19,8 @@ $(call inherit-product, vendor/xiaomi/picasso/picasso-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-cherish
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
