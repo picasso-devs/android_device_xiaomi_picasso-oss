4d9f2c6ad80cf33ee72980927800e40f22abdf12
itsurboimasarou <medicspam6@gmail.com>
2022-08-05 07:59:32 +0700

    picasso: Remove CameraGo
    We have shipped MIUICamera already so GcamGo isn't necessary anymore

--- device.mk
+++ device.mk
@@ -177,7 +177,6 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
     libgui_vendor \
-    CameraLite \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
